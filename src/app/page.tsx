ef70ab3e430b478453ae039446e4083bdb782a6d
Felpezs <felipe.edfreire@gmail.com>
2024-05-20 16:49:30 -0300

    feat: macrocards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,38 @@
 import CaloriesCard from "@/components/CaloriesCard";
+import MacroCard from "@/components/MacroCard";
 
 export default function Home() {
   return (
     <main className="mx-4 grid grid-cols-4 gap-8">
       <section className="col-span-4 mt-4">
-        <h1 className="bold mb-1 text-center text-2xl font-extrabold">
+        <h1 className="mb-1 text-center text-2xl font-extrabold">
           Hello Felipe!
         </h1>
-        <h2 className="text-center text-md ">What&apos;s your goal today ?</h2>
+        <h2 className="text-center text-md">What&apos;s your goal today ?</h2>
       </section>
-      <CaloriesCard />
+      <section className="col-span-full">
+        <CaloriesCard />
+      </section>
+      <section className="col-span-full flex gap-4">
+        <MacroCard
+          macroName="Proteins"
+          maxValue={300}
+          percentage={55}
+          value={175}
+        />
+        <MacroCard
+          macroName="Proteins"
+          maxValue={300}
+          percentage={55}
+          value={175}
+        />
+        <MacroCard
+          macroName="Proteins"
+          maxValue={300}
+          percentage={55}
+          value={175}
+        />
+      </section>
     </main>
   );
 }
---
+++ src/components/MacroCard.tsx
@@ -0,0 +1,23 @@
+const MacroCard = ({
+  macroName,
+  percentage,
+  value,
+  maxValue,
+}: {
+  macroName: string;
+  percentage: number;
+  value: number;
+  maxValue: number;
+}) => {
+  return (
+    <div className="flex-grow rounded-xl bg-surface-foreground p-4">
+      <h3 className="text-sm">{macroName}</h3>
+      <p className="text-lg font-extrabold">{percentage}%</p>
+      <p className="text-sm">
+        {value} of {maxValue}g
+      </p>
+    </div>
+  );
+};
+
+export default MacroCard;
