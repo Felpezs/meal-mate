092346ee6aed8b9ec48e7013f057417f1da9703d
Felpezs <felipe.edfreire@gmail.com>
2024-06-13 10:55:42 -0300

    fix: elements overlapping navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Logo from "./Logo";
 
 const Navbar = () => {
   return (
-    <nav className="fixed left-0 top-0 flex h-16 min-h-12 w-full items-center justify-between border-b-2 border-b-accent-500 bg-surface-light px-4 py-2 dark:bg-surface-dark">
+    <nav className="fixed left-0 top-0 z-50 flex h-16 min-h-12 w-full items-center justify-between border-b-2 border-b-accent-500 bg-surface-light px-4 py-2 dark:bg-surface-dark">
       <Logo />
       <Avatar />
     </nav>
